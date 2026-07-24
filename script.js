e987f7b73ac6f172c7adb2e55e869bcf4412c055
ffelectromecanica <ffmant.26@gmail.com>
2026-07-23 21:31:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const elementos = document.querySelectorAll("section, .card");
 
+const mostrarElemento = () => {
+
+    elementos.forEach(elemento => {
+
+        const posicion = elemento.getBoundingClientRect().top;
+        const alturaPantalla = window.innerHeight;
+
+        if (posicion < alturaPantalla - 100) {
+            elemento.style.opacity = "1";
+            elemento.style.transform = "translateY(0)";
+        }
+
+    });
+
+};
+
+
+elementos.forEach(elemento => {
+    elemento.style.opacity = "0";
+    elemento.style.transform = "translateY(40px)";
+    elemento.style.transition = "all 0.8s ease";
+});
+
+
+window.addEventListener("scroll", mostrarElemento);
+
+mostrarElemento();
